88ade63f62dd5b6970016a1aa5771bceaa04967c
Brendan <smallpigeon1@gmail.com>
2022-01-15 16:09:52 -0800

    Initial Commit

---
+++ Shapes/ShapesApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShapesApp.swift
+//  Shapes
+//
+//  Created by Brendan on 1/15/22.
+//
+
+import SwiftUI
+
+@main
+struct ShapesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
